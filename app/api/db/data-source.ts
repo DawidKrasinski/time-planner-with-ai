26ff0a0afb2527a6ca02e53e780dff85cf5672c7
Dawid Krasiński <dawidkrasinski14@gmail.com>
2025-08-08 13:56:19 +0200

    Added data-source

---
+++ app/api/db/data-source.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import "dotenv/config";
+import { BaseEntity, DataSource } from "typeorm";
+import { Task } from "./entity/Task";
+// import { Priority } from "./entity/Priority";
+// import { User } from "./entity/User";
+
+const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "3306"),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Task],
+  synchronize: true,
+  logging: false,
+});
+
+export const useDataSource = async () => {
+  if (!AppDataSource.isInitialized) await AppDataSource.initialize();
+  BaseEntity.useDataSource(AppDataSource);
+  return AppDataSource;
+};
