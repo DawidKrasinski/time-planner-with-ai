f9da161acbec911507d2876a9b99eca0502a31cd
Dawid Krasiński <dawidkrasinski14@gmail.com>
2025-08-14 23:51:43 +0200

    Added task list provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import TaskListProvider from "./taskListProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TaskListProvider>{children}</TaskListProvider>
       </body>
     </html>
   );
---
+++ app/taskListProvider.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { Task } from "../types/task";
+
+export type TaskListProvider = {};
+
+const TaskListContext = createContext<TaskListProvider | null>(null);
+
+export default function TaskListProvider(props: { children: React.ReactNode }) {
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  async function fetchTasks() {
+    const response = await fetch("/api/tasks");
+    const body = await response.json();
+    console.log("GET method response:", body);
+    setTaskList(body);
+  }
+
+  async function sendChatPrompt(prompt: string) {
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+    console.log("POST method response:", response);
+    fetchTasks();
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      <TaskListContext.Provider
+        value={{
+          taskList,
+          sendChatPrompt,
+        }}
+      >
+        {props.children}
+      </TaskListContext.Provider>
+    </div>
+  );
+}
+
+export function useTaskList() {
+  const context = useContext(TaskListContext);
+  if (!context) {
+    throw new Error("useToDoList must be used within a TodoListProvider");
+  }
+  return context;
+}
