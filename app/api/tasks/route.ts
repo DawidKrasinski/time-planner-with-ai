7e74611ff69480870fe9774446a53a0d0481ec8a
Dawid Krasiński <dawidkrasinski14@gmail.com>
2025-08-23 23:18:25 +0200

    Added get method

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { useDataSource } from "../db/data-source";
+import { Task } from "../db/entity/task";
+
+export async function GET() {
+  try {
+    await useDataSource();
+    const tasks = await Task.createQueryBuilder("task")
+      // .leftJoinAndSelect("task.priority", "priority")
+      .orderBy("task.doneDate", "ASC")
+      //  .addOrderBy("task.priority", "DESC")
+      .addOrderBy("task.startTime", "ASC")
+      .getMany();
+
+    return NextResponse.json(tasks);
+  } catch (error) {
+    console.log("cant use get method", error);
+    return NextResponse.json({ error: "cant use get method" }, { status: 500 });
+  }
+}
