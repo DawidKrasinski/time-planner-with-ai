0eaf840d5a68897095704123a3404532d89ce54b
Dawid Krasiński <dawidkrasinski14@gmail.com>
2025-09-16 16:22:19 +0200

    Refactor: improved Task entity

--- src/db/entities/Task.ts
+++ src/db/entities/Task.ts
@@ -5,6 +5,7 @@ import {
   PrimaryGeneratedColumn,
   BaseEntity,
   ManyToOne,
+  Index,
 } from "typeorm";
 import { Priority } from "./Priority";
 import { Categories } from "../../types/categories";
@@ -15,35 +16,39 @@ export class Task extends BaseEntity {
   id!: number;
 
   @Column()
+  @Index()
   name: string;
 
   @Column("datetime")
+  @Index()
   startTime: string;
 
   @Column("datetime")
+  @Index()
   endTime: string;
 
   @Column("simple-array")
   categories: Categories[];
 
   @Column("date", { nullable: true })
+  @Index()
   doneDate: string | null = null;
 
-  @ManyToOne(() => Priority, (priority) => priority.tasks)
-  priority: number;
+  @ManyToOne(() => Priority, (priority) => priority.tasks, { eager: false })
+  priority: Priority | null;
 
   constructor(
     name?: string,
-    priority?: number,
+    priority?: Priority,
     startTime?: string,
     endTime?: string,
     categories?: Categories[]
   ) {
     super();
     this.name = name ?? "";
+    this.priority = priority ?? null;
     this.startTime = startTime ?? "";
     this.endTime = endTime ?? "";
-    this.priority = priority ?? 0;
     this.categories = categories ?? [Categories.Microtask];
   }
 }
